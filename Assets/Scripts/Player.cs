40c71fc27af2dacc4d7c5b39d5b85f5469f0a02a
hms2002 <hmm2002@naver.com>
2022-05-05 02:27:08 +0900

    1. PlayerA애니메이션 적용 완료
    2. 이제 플레이어 스프라이트 크기에 상관 없이 점프, 설치 검사가 원활함
    3. UI 조금 만짐

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,10 @@ public class Player : MonoBehaviour
     KeyCode jumpKeyCode;
     KeyCode buildationKeyCode;
 
+    // 범용적으로 쓰이는 애니메이터, 스프라이트 변수 선언
+    Animator anim;
+    SpriteRenderer sprite;
+
 // 플레이어 이동 영역
     #region PlayerMove
 
@@ -30,12 +34,26 @@ public class Player : MonoBehaviour
     public void move()
     {
         xAxis = Input.GetAxisRaw(moveKeyName);
+        if(xAxis != 0)
+        {
+            if (xAxis > 0)
+                sprite.flipX = true;
+            else
+                sprite.flipX = false;
+            
+            anim.SetBool("isHorizontalZero", false);
+        }
+        else
+        {
+            anim.SetBool("isHorizontalZero", true);
+        }
 
         Vector2 vectorSpeed = new Vector2(xAxis * moveSpeed, rigid.velocity.y);
 
         rigid.velocity = vectorSpeed;
     }
     #endregion
+
 // 플레이어 점프 영역
     #region PlayerJump
     // #.플레이어 점프 관련 변수
@@ -50,21 +68,32 @@ public class Player : MonoBehaviour
     // #.플레이어 점프 관련 함수
     public bool canJump()
     {
-        fromJumpPoint = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
-        toJumpPoint = new Vector2(transform.position.x + 0.5f, transform.position.y - 0.6f);
+        // 점프 전 조사 할 영역 설정
+        float playerPosX = transform.position.x + playerBoxColider.offset.x;
+        float playerPosY = transform.position.y + playerBoxColider.offset.y;
+        // 플레이어의 콜라이더 바닥 y좌표값를 저장
+        float playerUnderPosY = playerPosY - playerBoxColider.size.y / 2f;
+
+        fromJumpPoint = new Vector2(playerPosX - playerBoxColider.size.x / 2f, playerUnderPosY);
+        toJumpPoint = new Vector2(playerPosX + playerBoxColider.size.x / 2f, playerUnderPosY - 0.1f);
         // raycast2D를 통한 바닥 검사
         Collider2D hit = Physics2D.OverlapArea(fromJumpPoint, toJumpPoint, mask);
 
         if (hit)
+        {
+            anim.SetBool("isGrounded", true);
             return true;
+        }
         else
+        {
+            anim.SetBool("isGrounded", false);
             return false;
+        }
     }
     public void jump()
     {
-        if (Input.GetKeyDown(jumpKeyCode))
+        if (canJump() && Input.GetKeyDown(jumpKeyCode))
         {
-            if (!canJump()) return;
 
             // 두 번 이어서 점프 했을 때 똑같은 높이로 점프하도록 y속도 초기화
             rigid.velocity = new Vector2(rigid.velocity.x, 0);
@@ -75,23 +104,75 @@ public class Player : MonoBehaviour
         }
     }
     #endregion
+
 // 플레이어 블럭 설치 영역
     #region PlayerBuildation
     // 설치 할 블럭 프리펩을 보관할 변수
     GameObject block;
 
-    // 블럭 설치가 가능한 영역인지 확인
-    GameObject checkBox;
+    // 플레이어 크기 가져올 boxCollider
+    BoxCollider2D playerBoxColider;
+
     // 설치 할 블럭의 크기 조사
     Vector2 fromBuildPoint;
     Vector2 toBuildPoint;
     // 설치할 수 있는 블럭 개수
     int blockCount;
 
+
+    // 블럭 설치 관련 함수들
+    public bool canBuild()
+    {
+        // 설치 전 조사 할 영역 설정
+        // + 추후 블럭 크기에 따라 달리지도록 수정해야 함
+        float playerPosX = transform.position.x + playerBoxColider.offset.x;
+        float playerPosY = transform.position.y + playerBoxColider.offset.y;
+        // 플레이어의 콜라이더 바닥 y좌표값를 저장
+        float playerUnderPosY = playerPosY - playerBoxColider.size.y / 2f;
+
+        float boxLength = 1f;
+        float boxHeight = 1f;
+
+        fromBuildPoint = new Vector2(playerPosX - boxLength / 2f, playerUnderPosY - 0.1f);
+        toBuildPoint = new Vector2(playerPosX + boxLength / 2f, playerUnderPosY - boxHeight);
+
+        Debug.Log("From : " + fromBuildPoint);
+        Debug.Log("To : " + toBuildPoint);
+
+        Collider2D hit = Physics2D.OverlapArea(fromBuildPoint, toBuildPoint);
+
+        if (hit)
+        {
+            Debug.Log(hit.name);
+            return false;
+        }
+        else return true;
+    }
+
+    public void build()
+    {
+        if (Input.GetKeyDown(buildationKeyCode))
+        {
+            if (!canBuild())
+            {
+                Debug.Log("불가능!");
+                return;
+            }
+            Vector2 position = new Vector2(transform.position.x, transform.position.y - 1);
+            Instantiate(block, position, transform.rotation);
+        }
+    }
+
     #endregion
 
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+
+        anim = GetComponent<Animator>();
+
+        playerBoxColider = GetComponent<BoxCollider2D>();
+
         block = Resources.Load<GameObject>("Prefabs/BlockDefault");
 
         rigid = GetComponent<Rigidbody2D>();
@@ -117,32 +198,5 @@ public class Player : MonoBehaviour
         move();
         jump();
         build();
-    }
-
-    public bool canBuild()
-    {
-        // 설치 전 조사 할 영역 설정
-        // + 추후 블럭 크기에 따라 달리지도록 수정해야 함
-        fromBuildPoint = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.55f);
-        toBuildPoint = new Vector2(transform.position.x + 0.5f, transform.position.y - 1.5f);
-
-        Collider2D hit = Physics2D.OverlapArea(fromBuildPoint, toBuildPoint);
-
-        if (hit) return false;
-        else return true;
-    }
-
-    public void build()
-    {
-        if(Input.GetKeyDown(buildationKeyCode))
-        {
-            if(!canBuild())
-            {
-                Debug.Log("불가능!");
-                return;
-            }
-            Vector2 position = new Vector2(transform.position.x, transform.position.y - 1);
-            Instantiate(block, position, transform.rotation);
-        }
     }
 }
